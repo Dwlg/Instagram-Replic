3387847c936bfbf1978304c1c57c54ff18e62317
Welington Serafim <serwelington@gmail.com>
2020-05-21 08:14:12 -0300

    Inicio do projeto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 p-5">
+            <img src="https://instagram.fsjk4-1.fna.fbcdn.net/v/t51.2885-19/11117047_1615846131986822_1837102719_a.jpg?_nc_ht=instagram.fsjk4-1.fna.fbcdn.net&_nc_ohc=PyqqKD4GarUAX8ZTP50&oh=ba6f5b702a87ea78b2396b57e6537971&oe=5EF00AC5" class="rounded-circle"/>
+        </div>
+        <div class="col-md-9 pt-5">
+            <div>
+                <h1>Nome do usuário</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5">
+                    <strong>x</strong> publicações
+                </div>
+                <div class="pr-5">
+                    <strong>x</strong> seguidores
+                </div>
+                <div class="pr-5">
+                    <strong>x</strong> seguindo
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
+@endsection
